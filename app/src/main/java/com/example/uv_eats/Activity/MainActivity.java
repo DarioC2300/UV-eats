9b9b8adbbd86e1bb2118351006b1f1596308e0fe
Axel Panama <axelpm28@gmail.com>
2022-11-09 13:03:12 -0600

    Se agrego el binding al ActivityMain

--- app/src/main/java/com/example/uv_eats/Activity/MainActivity.java
+++ app/src/main/java/com/example/uv_eats/Activity/MainActivity.java
@@ -1,24 +1,31 @@
 package com.example.uv_eats.Activity;
 
+import android.os.Bundle;
+
 import androidx.appcompat.app.AppCompatActivity;
+import com.example.uv_eats.databinding.ActivityMainBinding;
 
 
 public class MainActivity extends AppCompatActivity {
 
-    /*private ActivityLoginBinding binding;
+    private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        binding = ActivityLoginBinding.inflate(getLayoutInflater());
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
 
+        binding.ButtonLogin.setOnClickListener(v ->{
+            String username = binding.TextUser.toString();
+            String password = binding.TextPassword.toString();
+        });
 
         binding.ButtonRecoverPassword.setOnClickListener(v -> {
 
         });
-    }*/
+    }
 
 
 }

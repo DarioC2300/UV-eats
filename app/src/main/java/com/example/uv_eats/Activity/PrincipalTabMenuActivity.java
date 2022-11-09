94fdd990340eb20fc2690ba3563d34afd0685b0b
Samuel <samy.srzcln@gmail.com>
2022-11-09 11:18:34 -0600

    Main activity para la creacion del login

---
+++ app/src/main/java/com/example/uv_eats/Activity/PrincipalTabMenuActivity.java
@@ -0,0 +1,16 @@
+package com.example.uv_eats.Activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.uv_eats.R;
+
+public class PrincipalTabMenuActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_principal_tab_menu_activity);
+    }
+}
